b3fbd2cf85a5fdadb1c27e1e3a25866a33f09c2a
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-05-03 07:36:42 +0300

    Filling the page with components

---
+++ components/Car/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://pictures.porsche.com/rtt/iris?COSY-EU-100-1711coMvsi60AAt5FwcmBEgA4qP8iBUDxPE3Cb9pNXABuN9dMGF4tl3U0%25z8rMHIspMtwZaiEt5yPewjIRCvNzxvduGXoq1eH5r6FObyn5wRuT0cS2dioC1pqQDcFGbcXYn4Jq6GSeJ3Svna2xOq8Cu2gy9dfR6DrRQ"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Porsche Cayenne</h1>
+          <p class="text-gray-700">Lorem ipsum dolor sit amet consecteeur</p>
+        </div>
+        <h1 class="mt-auto text-xl">$39,555</h1>
+      </div>
+    </div>
+  </div>
+</template>
