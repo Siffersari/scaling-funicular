a6c9a2fd06879b8cb330bfbaab02cc1607264e07
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-05-03 09:00:56 +0300

    Unique Dynamic Paths

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam itaque
+      totam enim aperiam doloribus excepturi obcaecati maiores? Illum mollitia,
+      sapiente, eum iure quibusdam officiis ex explicabo eius quis harum
+      veritatis.
+    </p>
+  </div>
+</template>
---
+++ components/Car/Detail/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mt-10">
+    <img
+      src="https://pictures.porsche.com/rtt/iris?COSY-EU-100-1711coMvsi60AAt5FwcmBEgA4qP8iBUDxPE3Cb9pNXABuN9dMGF4tl3U0%25z8rMHIspMtwZaiEt5yPewjIRCvNzxvduGXoq1eH5r6FObyn5wRuT0cS2dioC1pqQDcFGbcXYn4Jq6GSeJ3Svna2xOq8Cu2gy9dfR6DrRQ"
+      alt=""
+      class="w-full"
+    />
+  </div>
+  <h1 class="mt-10 text-4xl">Porsche Cayenne</h1>
+  <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+    <div class="flex">
+      <p class="mr-2">5 seats</p>
+      <p class="mr-2">|</p>
+      <p class="mr-2">67,444 miles</p>
+    </div>
+
+    <div>
+      <p class="font-bold text-2xl">£ 35,555</p>
+    </div>
+  </div>
+</template>
