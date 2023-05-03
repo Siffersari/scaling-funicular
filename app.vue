610168b32526b7c1140e540f04a484a2c0fd07d6
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-05-03 06:04:44 +0300

    Build the home hero

--- app.vue
+++ app.vue
@@ -7,5 +7,20 @@
       <a class="text-3xl font-mono" href="">cartrader</a>
     </header>
     <!-- NAVBAR -->
+
+    <!-- HOME HERO -->
+    <!-- https://pictures.porsche.com/rtt/iris?COSY-EU-100-1711coMvsi60AAt5FwcmBEgA4qP8iBUDxPE3Cb9pNXABuN9dMGF4tl3U0%25z8rMHIspMtwZaiEt5yPewjIRCvNzxvduGXoq1eH5r6FObyn5wRuT0cS2dioC1pqQDcFGbcXYn4Jq6GSeJ3Svna2xOq8Cu2gy9dfR6DrRQ -->
+    <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://assets.whichcar.com.au/image/private/s--Jfxwc9oa--/c_fill,q_auto:good/c_scale,w_3072/t_w_wheels_sw/f_auto/2022_mercedes_benz_a45_amg_hatch_yellow_63.jpg')]"
+    >
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+
+    <!-- HOME HERO -->
   </div>
 </template>
